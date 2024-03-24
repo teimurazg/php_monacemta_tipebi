1894e8922175e1420cdbf07520004236f101ceef
temo <teimurazi.g@gmail.com>
2024-03-25 02:30:39 +0400

    monacemta tipebi

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+
+$string = "temo";
+
+echo var_dump($string). "<br/>";
+
+
+
+$integer = 33;
+
+echo var_dump($integer). "<br/>";
+
+
+
+$float = 33.5;
+
+echo var_dump($float). "<br/>";
+
+
+
+$boolean = 5 > 2;
+
+echo var_dump($boolean). "<br/>";
+
+
+
+$temo = 50;
+
+echo var_dump($temo). "<br/>";
+
+
+
+$temosAge = (string)$temo;
+
+echo var_dump($temosAge). "<br/>";
+
+
+define("TEMO", 77);
+
+echo var_dump(TEMO). "<br/>";
+
+
+$carCompany = ["Audi", "BMW", "Mercedes", "Jaguar"];
+
+echo var_dump($carCompany[1]);
+
+
+
+
+
+?>
